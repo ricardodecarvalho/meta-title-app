ee00021adc7c38c10624054dd09c8178f53929b4
Ricardo de Carvalho <ricardo.carvalho@yes.technology>
2025-04-13 22:16:45 -0300

    feat: Change Open graph

--- public/env.js
+++ public/env.js
@@ -1,14 +1,7 @@
 (function () {
-  const path = window.location.pathname;
-
-  const titles = {
-    "/": "Home - Vite App",
-    "/about": "About Us - Vite App",
-    "/contact": "Contact - Vite App",
-    "/admin": "Admin Panel - Vite App",
-  };
-
   window.APP_CONFIG = {
-    PAGE_TITLE: titles[path] || "Vite App",
+    PAGE_TITLE: "Custom Title Vite App",
+    PAGE_DESCRIPTION:
+      "This is a sample Vite application demonstrating dynamic meta tags.",
   };
 })();
